412c4f9ab4a098d4fd51d7af188933650327ce66
pratik@gmail.com <userone@gmail.com>
2024-01-30 18:31:40 +0530

    First commit

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class LoginPage {
+
+	public void logintoapp() {
+		
+		System.out.println("login to application");
+		
+	}
+	
+	public void fetchtitle() {
+		
+		System.out.println(" getting title of page");
+		
+	}
+}
